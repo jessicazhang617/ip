eed5f72e59fe042a77dbf1f1acf7eba53e239201
jessicazhang617 <47000098+jessicazhang617@users.noreply.github.com>
2020-09-18 00:02:25 +0800

    Resolve merge conflicts

--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -1,10 +1,6 @@
 package duke.task;
 
-<<<<<<< HEAD
-public class Event extends Task {
-=======
 public class Event extends Task{
->>>>>>> ba2b3ec3e061b93a4655ccd47b469081fe7822e0
     /** Date and time.*/
     private String taskDeadline;
 
